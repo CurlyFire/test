548efed10e233223e91f2bc7ef8102f7e27732d5
CurlyFire <sd@virtuso.ca>
2020-03-24 07:52:44 -0400

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+        }
+
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Fail();
+        }
+
+    }
+}
